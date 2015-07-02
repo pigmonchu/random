f7fd1ea1e50e4e97dd07574b893f6a8d7677600d
pigmonchu <pigmonchu@gmail.com>
2015-07-02 09:28:12 +0200

    Ejercicio 5, mooc node.
    Añadido igualdad a 0,5, readme y license.

--- random.js
+++ random.js
@@ -4,7 +4,9 @@ var numero = Math.random();
 var str =' MAYOR que 0,5';
 
 if (numero <= 0.5){
-  str = ' MENOR que 0,5';
+	str = ' MENOR que 0,5';
+} else (numero === 5) {
+	str = ' IGUAL que 0,5'; //OPCIÓN CON PROBABILIDAD PRÁCTICAMENTE NULA
 }
 
 console.log('\n' + numero + str + '\n');
---
+++ readme.md
@@ -0,0 +1,6 @@
+PROGRAMA RANDOM
+===============
+
+Este programa genera un número aleatorio entre 0 y 1 e informa si es mayor, menor o igual que 0,5.
+
+Aunque se pregunta por valor igual a 0,5, la **probabilidad** de que esto ocurra es **mínima**.
